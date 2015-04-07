81f3d41d607515e1fa99efea59988386d6e8cccd
Neal Bhasin <thisisneal@gmail.com>
2015-04-07 18:32:47 -0400

    Working divert guidance! Limits and optimizes acceleration instead of thrust for linearity.

--- p1_simple.m
+++ p1_simple.m
@@ -1,10 +1,13 @@
-%
+% [x ; z]
 
 % Vehicle fixed parameters
 m_dry = 1505;
 Isp = 225;
-g = [0 ; -3.7114];
-T_max = 6 * 3100;
+g0 = 9.80665; % Standard earth gravity
+g = [0 ; -3.7114]; % Mars gravity vector
+max_throttle = 0.8;
+T_max = 6 * 3100 * max_throttle;
+phi = 27; % The cant angle of thrusters in degrees
 
 % Initial conditions
 m_wet = 1905;
@@ -15,19 +18,18 @@ v0 = [100 ; -75];
 rf = [ 0 ; 0 ];
 vf = [ 0 ; 0];
 
-N = tf + 1; % 1 hz discrete time nodes
-tf = 72;
-dt = tf / (N - 1);
-tv = 0:dt:N;
+tf = 75;
+dt = 1; % 1 hz discrete time nodes
+N = (tf / dt) + 1;
+tv = 0:dt:tf;
 
 cvx_begin
-    variables r(2,N) T(2,N) m(1,N)
+    variables r(2,N) a(2,N)
     
-    maximize( m(1,N) )
+    minimize( norm(a) )
     
     subject to
         % Initial condition constraints
-        m(1) == m_wet;
         r(:,1) == r0;
         r(:,2) == r0 + v0 * dt;
         % Terminal condition constraints
@@ -36,14 +38,45 @@ cvx_begin
         % Dynamical constraints
         for i=3:N
             % Acceleration
-            %(r(:,i) - 2*r(:,i-1) + r(:,i-2))/dt^2 == g + T(:,i) / m(i);
-            (r(:,i) - 2*r(:,i-1) + r(:,i-2))/dt^2 == g + T(:,i) / m_wet;
-            m(i) <= m(i-1);
+            (r(:,i) - 2*r(:,i-1) + r(:,i-2))/dt^2 == g + a(:,i);
+            norm(a(:,i)) <= T_max / m_wet; % Very conservative 
         end
+        % No sub-surface flight
+        r(2,:) >= -1;
 cvx_end
+
+a_norms = norms(a);
+a_dirs = rad2deg(atan2(a(2,:), a(1,:)));
+
+% Compute vehicle mass over time
+m = zeros(1, N);
+m(1) = m_wet;
+for i=1:N-1
+    T_i = a_norms(i) * m(i);
+    m_dot = -T_i / (Isp * g0 * cosd(phi));
+    m(i+1) = m(i) + m_dot * dt;
+end
 
 figure;
 plot(tv, r(1,:), tv, r(2,:));
+title('Position (m)');
+
+figure;
+plot(r(1,:), r(2,:));
+title('Trajectory (m)');
+
+figure;
+plot(tv, a(1,:), tv, a(2,:));
+title('Acceleration (m/s^2)');
+
+figure;
+plot(tv, a_norms);
+title('Acceleration (m/s^2)');
+
+figure;
+plot(tv, a_dirs);
+title('Acceleration (deg)');
 
 figure;
 plot(tv, m);
+title('Mass (kg)');
