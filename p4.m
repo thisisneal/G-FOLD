cae5d1d48020995d72293ec19003ed751fcafb61
Neal Bhasin <thisisneal@gmail.com>
2015-04-21 21:38:02 -0400

    Add 2D glide slope example.

--- p4.m
+++ p4.m
@@ -90,43 +90,12 @@ cvx_begin
         end
         % No sub-surface flight
         r(2,:) >= -1;
+        % Glide-slope surface constraint
+        %slope = 4;
+        %r(1,:) <= r(2,:) / tand(slope);
 cvx_end
 
 % Plotting
-u_norms = norms(u);
-u_dirs = rad2deg(atan2(u(2,:), u(1,:)));
+
 m_vals = exp(z);
-T_vals = u_norms .* m_vals;
-
-figure; hold on;
-plot(tv, r(1,:), tv, r(2,:));
-title('Position (m)');
-
-figure; hold on;
-plot(tv, v(1,:), tv, v(2,:));
-title('Velocity (m/s)');
-
-figure; hold on;
-plot(r(1,:), r(2,:));
-quiver(r(1,:), r(2,:), u(1,:), u(2,:), .25);
-title('Trajectory (m)');
-
-figure;
-plot(tv, u(1,:), tv, u(2,:));
-title('Acceleration (m/s^2)');
-
-figure; hold on;
-plot(tv, T_vals);
-title('Thrust (N)');
-
-figure;
-plot(tv, u_norms);
-title('Acceleration (m/s^2)');
-
-figure;
-plot(tv, u_dirs);
-title('Acceleration (deg)');
-
-figure;
-plot(tv, exp(z));
-title('Mass (kg)');
+plot_run2D(tv,r,v,u,m_vals);
